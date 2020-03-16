4a9887e214a10f6bcacfc576e053522f12b47391
Mateus Mangerona <mateus.mangerona@hotmail.com>
2020-03-16 18:41:36 -0300

    adicionando css in index e filanizando o css do menu

---
+++ src/pages/ProductsDetails/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './index.module.css';
+
+function ProductsList() {
+  return (
+    <div className={styles.productList}>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+    </div>
+  );
+}
+
+export default ProductsList;
--- src/pages/ProductsList/index.jsx
+++ src/pages/ProductsList/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import styles from './index.module.css';
 
 function ProductsList() {
   return (
-    <>
-      oi
-    </>
+    <div className={styles.productList}>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+      <p> oi </p>
+    </div>
   );
 }
 
