e59fb5df928cbd0733ac8fecdab0b8718753822a
Mateus Mangerona <mateus.mangerona@hotmail.com>
2020-03-16 22:02:44 -0300

    adicionando compenente de titulo e chamadas de querys

--- src/pages/ProductsList/index.jsx
+++ src/pages/ProductsList/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import styles from './index.module.css';
+import Title from '../../components/Header/Title';
+import { PAGE_TITLE, PAGE_SUBTITLE, PAGE_DESCRIPTION } from '../../enum';
+import { useQuery } from '../../hooks';
 
 function ProductsList() {
+  const query = useQuery;
+  const getTitle = () => PAGE_TITLE[query().get('query')] || PAGE_TITLE.all;
+  const getSubTitle = () => PAGE_SUBTITLE[query().get('query')] || PAGE_SUBTITLE.all;
+  const getDescription = () => PAGE_DESCRIPTION[query().get('query')] || PAGE_DESCRIPTION.all;
   return (
     <div className={styles.productList}>
-      <p> oi </p>
-      <p> oi </p>
-      <p> oi </p>
-      <p> oi </p>
-      <p> oi </p>
-      <p> oi </p>
-      <p> oi </p>
-      <p> oi </p>
+      <Title title={getTitle()} subTitle={getSubTitle()} description={getDescription()} />
     </div>
   );
 }
