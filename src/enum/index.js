318d65da01b507898d237e75a6d847054c1a4609
Mateus Mangerona <mateus.mangerona@hotmail.com>
2020-03-16 22:00:46 -0300

    criação de enum para textos staticos

---
+++ src/enum/index.js
@@ -0,0 +1,26 @@
+export const PAGE_TITLE = {
+  all: 'EMPRESA XPTO',
+  exclusives: 'EMPRESA XPTO',
+  promotion: 'EMPRESA XPTO',
+  favorites: 'EMPRESA XPTO',
+};
+
+export const PAGE_SUBTITLE = {
+  all: 'Conheça todos os nossos produtos',
+  exclusives: 'Conheça nossos produtos exclusivos',
+  promotion: 'Conheça nossas promoções',
+  favorites: 'Meus Favoritos',
+};
+
+export const PAGE_DESCRIPTION = {
+  all: 'Listagem de produtos - clique no produto desejado para saber mais',
+  exclusives: 'Listagem de produtos exclusivos - clique no produto desejado para saber mais',
+  promotion: 'Listagem de produtos em promoção - clique no produto desejado para saber mais',
+  favorites: 'Listagem de produtos marcados como favoritos - clique no produto desejado para saber mais',
+};
+
+export default {
+  PAGE_TITLE,
+  PAGE_SUBTITLE,
+  PAGE_DESCRIPTION,
+};
