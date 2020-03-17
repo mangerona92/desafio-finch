a808bb4af1a7bf551e76096135deb5c37272276d
Mateus Mangerona <mateus.mangerona@hotmail.com>
2020-03-16 22:00:06 -0300

    criação do header/title adicionando proptypes no projeto

---
+++ src/components/Header/Search/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './index.module.css';
+
+function Title(props) {
+  const { title, subTitle, description } = props;
+  return (
+    <>
+      <div className={styles.title}>
+        <h2>
+          { title }
+        </h2>
+        <h2 className={styles.subTitle}>
+          -
+          { subTitle }
+        </h2>
+      </div>
+      <span className={styles.description}>
+        { description }
+      </span>
+    </>
+  );
+}
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default Title;
